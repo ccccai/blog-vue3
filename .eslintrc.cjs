db3daa0e92a705a466b86975a681304e64f09e47
cc <cc@123.com>
2024-02-02 17:26:15 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,38 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2020: true
+  },
+  rules: {
+    'no-console': import.meta.env.MODE === 'production' ? 'warn' : 'off',
+    'no-debugger': import.meta.env.MODE === 'production' ? 'warn' : 'off',
+    'vue/require-default-prop': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'vue/html-self-closing': ['off'],
+    'vue/singleline-html-element-content-newline': ['off'],
+    'vue/html-closing-bracket-newline': ['off'],
+    'vue/html-indent': 'off',
+    '@typescript-eslint/no-this-alias': 'off'
   }
 }
